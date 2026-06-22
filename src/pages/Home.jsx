ad1631bc78b0a9d3802a2eec41a538e78587f80a
Aakif-Kohari <kohariaakif@gmail.com>
2026-06-22 19:32:33 +0530

    Initial commit - vite + react + tailwind setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+
+function Home() {
+  return (
+    <div className="min-h-screen bg-[#000000]">
+      <Navbar />
+      <Hero />
+    </div>
+  )
+}
+
+export default Home
