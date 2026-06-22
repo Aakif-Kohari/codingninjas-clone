80d5440e7b06603c034dcc44bae1dc01d2aa2557
Aakif-Kohari <kohariaakif@gmail.com>
2026-06-22 19:33:16 +0530

    Add StatsBar with placeholder numbers

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import StatsBar from '../components/StatsBar'
 
 function Home() {
   return (
     <div className="min-h-screen bg-[#000000]">
       <Navbar />
       <Hero />
+      <StatsBar />
     </div>
   )
 }
