a3b52a02d20b823642bbf839a1624d2a76de7c9d
Aakif-Kohari <kohariaakif@gmail.com>
2026-06-23 11:55:05 +0530

    Add WhyUs section with feature icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@ import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import StatsBar from '../components/StatsBar'
 import CoursesSection from '../components/CoursesSection'
+import WhyUs from '../components/WhyUs'
+import Testimonials from '../components/Testimonials'
+import Footer from '../components/Footer'
 
 function Home() {
   return (
@@ -10,6 +13,9 @@ function Home() {
       <Hero />
       <StatsBar />
       <CoursesSection />
+      <WhyUs />
+      <Testimonials />
+      <Footer />
     </div>
   )
 }
